f2c1456aba98c594aa2c5e90ff03e58cdc6e2ff0
Alfian <Scrip@DESKTOP-MC2VKDF>
2020-07-23 13:26:22 +0800

    Update Camcorder.java and Chromebook.java

--- ResourceCentre_Student/src/Chromebook.java
+++ ResourceCentre_Student/src/Chromebook.java
@@ -12,8 +12,7 @@ public class Chromebook extends Item{
 	}
 	
 	public String toString(){
-		String output = "";
-		// Write your codes here
+		String output = "Asset Tag: " + getAssetTag() + "\n Description:- " + getDescription() ;
 		return output;
 	}
 }
